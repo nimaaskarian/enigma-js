e07d430c5c93caf8d4daae7c60b1f143b20977dd
NimaAskarian <nimaas1400@gmail.com>
2022-01-07 15:44:59 +0330

    ability to copy the text by pressing enter

--- js/app.js
+++ js/app.js
@@ -1,19 +1,52 @@
 import Enigma from "./enigma.js";
-import { alphabetsToPositionArray, positionToAlphabet } from "./utils.js";
+import {
+  alphabetsToPositionArray,
+  positionToAlphabet,
+  copyTextToClipboard,
+} from "./utils.js";
 
-let req = new XMLHttpRequest()
-req.open("GET", "../settings.json",false)
-req.send(null)
+let req = new XMLHttpRequest();
+req.open("GET", "../settings.json", false);
+req.send(null);
 const settings = JSON.parse(req.responseText);
 
 const input = document.querySelector(".text-input");
 const output = document.querySelector(".text-output");
 
-input.addEventListener("input", ({ currentTarget: { value } }) => {
+function encryptInputAndShowOutput(input) {
   const enigma = new Enigma(settings);
-  const encryptedArray = alphabetsToPositionArray(value).map((pos) => {
+  const encryptedArray = alphabetsToPositionArray(input).map((pos) => {
     if (pos === -1) return;
     return positionToAlphabet(enigma.encrypt(pos));
   });
   output.innerHTML = encryptedArray.toString().replace(/,/g, "");
+}
+//event listeners
+window.addEventListener("load", () => {
+  input.focus();
+  input.setSelectionRange(input.value.length, input.value.length);
+  encryptInputAndShowOutput(input.value);
+});
+
+input.addEventListener("keydown", ({ key }) => {
+  if (key === "Enter") {
+    copyTextToClipboard(
+      output.innerText,
+      () => {
+        document.querySelector(".alert-copy").style.opacity = 1;
+        setTimeout(() => {
+          document.querySelector(".alert-copy").style.opacity = 0;
+        }, 1500);
+      },
+      () => {
+        document.querySelector(".alert-error").style.opacity = 1;
+        setTimeout(() => {
+          document.querySelector(".alert-error").style.opacity = 0;
+        }, 1500);
+      }
+    );
+  }
+});
+input.addEventListener("input", ({ currentTarget: { value } }) => {
+  encryptInputAndShowOutput(value);
 });
--- js/utils.js
+++ js/utils.js
@@ -26,3 +26,38 @@ export function rotorConstructor() {
   }
   return [...wires];
 }
+
+function fallbackCopyTextToClipboard(text) {
+  var textArea = document.createElement("textarea");
+  textArea.value = text;
+
+  // Avoid scrolling to bottom
+  textArea.style.top = "0";
+  textArea.style.left = "0";
+  textArea.style.position = "fixed";
+
+  document.body.appendChild(textArea);
+  textArea.focus();
+  textArea.select();
+
+  try {
+    var successful = document.execCommand("copy");
+    var msg = successful ? "successful" : "unsuccessful";
+    console.log("Fallback: Copying text command was " + msg);
+  } catch (err) {
+    console.error("Fallback: Oops, unable to copy", err);
+  }
+
+  document.body.removeChild(textArea);
+}
+export function copyTextToClipboard(
+  text,
+  onCopy = new Function(),
+  onError = new Function()
+) {
+  if (!navigator.clipboard) {
+    fallbackCopyTextToClipboard(text);
+    return;
+  }
+  navigator.clipboard.writeText(text).then(onCopy, onError);
+}
